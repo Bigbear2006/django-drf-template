b0dd29d4c0420be39eab66f0da5219b83ec5ff64
Михаил <morozm982@gmail.com>
2024-11-26 17:08:44 +0300

    added swagger, changed backend Dockerfile

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,11 +18,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from drf_yasg.views import get_schema_view
+from drf_yasg.openapi import Info
+
+view = get_schema_view(Info("API", "v1", "API description"), public=True)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("api.urls")),
     path('api/auth/', include("jwt_auth.urls")),
+    path('api/docs/', view.with_ui("swagger")),
 ]
 
 if not settings.DOCKER:
